20a7e4e7a861b450a9ae5eefcbada5a3cc70c817
Tony <hzx.tony@hotmail.com>
2022-01-09 16:25:23 +0800

    add EventLinstener

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,8 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  Alert,
   SafeAreaView,
   ImageBackground,
   StatusBar,
@@ -19,107 +18,105 @@ import {
   useColorScheme,
   View,
   TouchableHighlight,
-  Image
+  Image,
+  NativeEventEmitter,
+  NativeModules,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Bar from 'react-native-progress/Bar';
-setInterval(() => {
-
-
-
-
-}, 1000);
-const Section: React.FC<{
-  title: string;
-}> = ({ children, title }) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [isOpenNeteaseMusic, setIsOpenNeteaseMusic] = useState(false);
+  const [isConnectToPC, setIsConnectToPC] = useState(false);
+  useEffect(() => {
+    console.log('addEventLinster')
+    const eventEmitter = new NativeEventEmitter(NativeModules.AndroidSender);
+    const eventListener = eventEmitter.addListener(
+      'EVENT_SET_PC_STATE',
+      event => {
+        console.log(event.eventProperty); // "someValue"
+      },
+    );
+    return eventListener.remove();
+  });
   return (
-    
     <SafeAreaView>
-      <StatusBar barStyle='light-content' backgroundColor='rgba(0,0,0,0)' translucent={true}></StatusBar>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="rgba(0,0,0,0)"
+        translucent={true}
+      />
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ImageBackground source={require('./assets/background.png')} style={styles.appContainer}>
-        <Text style={styles.titleWrapper}>運命 ～GRAND BATTLE～ (orchestra ver.)</Text>
-        <Text style={styles.subtitleWrapper}>深澤秀行 / 三宅一徳 / 東京都交響楽団</Text>
-        <View style={styles.albumContainer} >
-         <Image   style={styles.albumImage}   source={{uri: 'http://p2.music.126.net/FGYCo9ZtZd-vcOBZXeu-Cg==/109951165868160762.jpg'}}>
-          </Image> 
+      <ImageBackground
+        source={require('./assets/background.png')}
+        style={styles.appContainer}>
+        <Text style={styles.titleWrapper}>
+          運命 ～GRAND BATTLE～ (orchestra ver.)
+        </Text>
+        <Text style={styles.subtitleWrapper}>
+          深澤秀行 / 三宅一徳 / 東京都交響楽団
+        </Text>
+        <View style={styles.albumContainer}>
+          <Image
+            style={styles.albumImage}
+            source={{
+              uri: 'http://p2.music.126.net/FGYCo9ZtZd-vcOBZXeu-Cg==/109951165868160762.jpg',
+            }}
+          />
         </View>
         <View style={styles.statusIndicatorWrapper}>
           <View style={styles.statusIndicator}>
-            <MaterialIcons name={'done'} size={30} color="#7fa37b" />
+            <MaterialIcons
+              name={isConnectToPC ? 'done' : 'close'}
+              size={30}
+              color={isConnectToPC ? '#7fa37b' : '#8c0000'}
+            />
             <Text style={styles.statusIndicatorText}>已连接PC</Text>
           </View>
           <View style={styles.statusIndicator}>
-            <MaterialIcons name={'close'} size={30} color="#8c0000" />
+            <MaterialIcons
+              name={isOpenNeteaseMusic ? 'done' : 'close'}
+              size={30}
+              color={isOpenNeteaseMusic ? '#7fa37b' : '#8c0000'}
+            />
             <Text style={styles.statusIndicatorText}>网易云未运行</Text>
           </View>
         </View>
         <View style={styles.progressBarWrapper}>
-          <Bar progress={0.15} width={300} color="#a8a8a8" height={8}  unfilledColor="rgba(255,255,255,0.2)" borderWidth={0} />
+          <Bar
+            progress={0.15}
+            width={300}
+            color="#a8a8a8"
+            height={8}
+            unfilledColor="rgba(255,255,255,0.2)"
+            borderWidth={0}
+          />
         </View>
         <View style={styles.controlerWrapper}>
-          <MaterialIcons 
-          onPress={() => {
-            fetch(
-              'http://192.168.2.146:9283/media/prev'
-            );
-            
-          }}
-          name={'skip-previous'} size={40} color="#000" />
-          <TouchableHighlight style={styles.palyControler}
-          onPress={() => {
-            fetch(
-              'http://192.168.2.146:9283/media/play'
-            );
-            
-          }}
-          >
+          <MaterialIcons
+            onPress={() => {
+              fetch('http://192.168.2.146:9283/media/prev');
+            }}
+            name={'skip-previous'}
+            size={40}
+            color="#000"
+          />
+          <TouchableHighlight
+            style={styles.palyControler}
+            onPress={() => {
+              fetch('http://192.168.2.146:9283/media/play');
+            }}>
             <MaterialIcons name={'play-arrow'} size={50} color="#000" />
           </TouchableHighlight>
-          <MaterialIcons 
-          onPress={() => {
-            fetch(
-              'http://192.168.2.146:9283/media/next'
-            );
-            
-          }}
-          
-          name={'skip-next'} size={40} color="#000" />
+          <MaterialIcons
+            onPress={() => {
+              fetch('http://192.168.2.146:9283/media/next');
+            }}
+            name={'skip-next'}
+            size={40}
+            color="#000"
+          />
         </View>
       </ImageBackground>
     </SafeAreaView>
@@ -131,16 +128,16 @@ const styles = StyleSheet.create({
     height: '100%',
     backgroundColor: '#fff',
     display: 'flex',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   titleWrapper: {
-    fontWeight:'500',
+    fontWeight: '500',
     fontSize: 27,
     color: '#000',
     marginTop: 80,
     textShadowColor: 'rgba(11, 11, 11,0.3)',
     textShadowOffset: {width: 0, height: 0},
-    textShadowRadius: 9
+    textShadowRadius: 9,
   },
   subtitleWrapper: {
     fontSize: 14,
@@ -149,59 +146,56 @@ const styles = StyleSheet.create({
     marginTop: 1,
     textShadowColor: 'rgba(3, 3, 3,0.19)',
     textShadowOffset: {width: -1, height: -1},
-    textShadowRadius: 5
+    textShadowRadius: 5,
   },
   albumContainer: {
-    position:'relative',
+    position: 'relative',
     width: 250,
     height: 250,
     backgroundColor: '#fff',
     borderRadius: 20,
     marginTop: 40,
     elevation: 22,
-    zIndex:199,
-    overflow:'hidden'
-    
+    zIndex: 199,
+    overflow: 'hidden',
   },
   albumImage: {
-    position:'relative',
-    
-    zIndex:1,
-    transform: [{ scale: 1.09 }],
-    flex:1,
-    
-    resizeMode: 'cover'
-    
+    position: 'relative',
 
+    zIndex: 1,
+    transform: [{scale: 1.09}],
+    flex: 1,
+
+    resizeMode: 'cover',
   },
   statusIndicatorWrapper: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: '100%',
-    marginTop: 50
+    marginTop: 50,
   },
   statusIndicator: {
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   statusIndicatorText: {
     fontSize: 20,
     color: '#000',
-    fontWeight: '500'
+    fontWeight: '500',
   },
   progressBarWrapper: {
-    marginTop: 30
+    marginTop: 30,
   },
   controlerWrapper: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    bottom:-150,
-    // marginTop: -500,
-    width: 250
+    // bottom: -150,
+    marginTop: 10,
+    width: 250,
   },
   palyControler: {
     backgroundColor: '#fff',
@@ -213,7 +207,7 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     // borderWidth: 1,
     borderColor: '#999',
-    elevation: 20
+    elevation: 20,
   },
   sectionContainer: {
     marginTop: 32,
@@ -232,9 +226,5 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
 });
-
-const titleStyle = StyleSheet.create({
-
-})
 
 export default App;
